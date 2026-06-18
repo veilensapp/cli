d14b9a1459879892dc6717f15ace734478d39b15
Marius S <39998+winding-lines@users.noreply.github.com>
2026-06-18 07:14:46 -0700

    homebrew: bump formula to v0.1.4

--- dist/homebrew/veilens.rb
+++ dist/homebrew/veilens.rb
@@ -3,9 +3,9 @@ class Veilens < Formula
   homepage "https://github.com/veilensapp/cli"
   # version / url / sha256 are bumped per release by dist/homebrew/update-formula.sh
   # (downloads the veilens-macos.tar.gz release asset and fills in its checksum).
-  version "0.1.3"
-  url "https://github.com/veilensapp/cli/releases/download/v0.1.3/veilens-macos.tar.gz"
-  sha256 "a31860d72b27457bbad06d28ddeee71e83e0b546b65412068952b7106dd1941b"
+  version "0.1.4"
+  url "https://github.com/veilensapp/cli/releases/download/v0.1.4/veilens-macos.tar.gz"
+  sha256 "9442d4917a41f20a97b72444d5dff1ee6a699abf28a8c64ccac19d48e3c18476"
 
   depends_on :macos
 
